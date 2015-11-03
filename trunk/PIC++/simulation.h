19d9167d99a3ebd435bcca4c917902fd264f5abf
vadimromansky <vadimromansky@de5680a3-3ee4-4deb-a3dd-9efdbc3f641f>
2015-11-03 16:54:11 +0000

    rotated electromagnetic wave

--- trunk/PIC++/simulation.h
+++ trunk/PIC++/simulation.h
@@ -188,6 +188,7 @@ public:
 
 	void initialize();
 	void initializeSimpleElectroMagneticWave();
+	void initializeRotatedSimpleElectroMagneticWave();
 	void checkFrequency(double omega);
 	void initializeAlfvenWave(int wavesCount, double amplitudeRelation);
 	void initializeRotatedAlfvenWave(int wavesCount, double amplitudeRelation);
