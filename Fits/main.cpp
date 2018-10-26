e804a5abf205614286ad4ec24d80ea2e43b4442f
vadim <romanskyvadim@gmail.com>
2018-10-26 17:12:20 +0300

    1d array

--- Fits/main.cpp
+++ Fits/main.cpp
@@ -1,16 +1,18 @@
 // Fits1.cpp : Defines the entry point for the console application.
 //
 
-#include "../cfitsio/include/fitsio.h"
+#include "../../cfitsio/include/fitsio.h"
 #include <cstdio>
 
 int main() {
 	const int Nx = 4001;
 	const int Ny = 101;
-	double** image = new double*[Nx];
+	long** image = new long*[Nx];
 	for (int i = 0; i < Nx; ++i) {
-		image[i] = new double[Ny];
+		image[i] = new long[Ny];
 	}
+
+    long* image2 = new long[Nx*Ny];
 
 	FILE* input = fopen("../B.dat", "r");
 	for (int i = 0; i < Nx; ++i) {
@@ -25,31 +27,35 @@ int main() {
 	fitsfile* fptr; /* pointer to the FITS file; defined in fitsio.h */
 	int status, ii, jj;
 	long fpixel = 1, naxis = 2, nelements, exposure;
-	long naxes[2] = {300, 200}; /* image is 300 pixels wide by 200 rows */
-	short array[200][300];
+	long naxes[2] = {Ny, Nx}; /* image is 300 pixels wide by 200 rows */
+	int array[Nx][Ny];
 	status = 0; /* initialize status before calling fitsio routines */
-	fits_create_file(&fptr, "../B.fits", &status); /* create new file */
+	fits_create_file(&fptr, "!../B.fits", &status); /* create new file */
 	/* Create the primary array image (16-bit short integer pixels */
-	fits_create_img(fptr, SHORT_IMG, naxis, naxes, &status);
+	fits_create_img(fptr, LONG_IMG, naxis, naxes, &status);
 	/* Write a keyword; must pass the ADDRESS of the value */
 	exposure = 1500.;
 	fits_update_key(fptr, TLONG, "EXPOSURE", &exposure, "Total Exposure Time", &status);
 	/* Initialize the values in the image with a linear ramp function */
-	for (jj = 0; jj < naxes[1]; jj++)
-		for (ii = 0; ii < naxes[0]; ii++)
-			array[jj][ii] = ii + jj;
+	for (jj = 0; jj < naxes[1]; jj++) {
+        for (ii = 0; ii < naxes[0]; ii++) {
+            //array[jj][ii] = ii + jj;
+            //image[jj][ii] = array[jj][ii];
+            image2[ii + jj*naxes[0]] = image[jj][ii];
+        }
+    }
 
 	nelements = naxes[0] * naxes[1]; /* number of pixels to write*/
 	/* Write the array of integers to the image */
-	fits_write_img(fptr, TSHORT, fpixel, nelements, array[0], &status);
+	fits_write_img(fptr, TLONG, fpixel, nelements, image2, &status);
 	fits_close_file(fptr, &status); /* close the file */
 	fits_report_error(stderr, status); /* print out any errormessages */
-	return (status);
 
 	for (int i = 0; i < Nx; ++i) {
 		delete[] image[i];
 	}
 	delete[] image;
-	return 0;
+    delete[] image2;
+    return (status);
 }
 
