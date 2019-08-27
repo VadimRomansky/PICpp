ee0b280f920d52893dfcd035cd2efa2e47a74fbe
vadimromansky <romanskyvadim@gmail.com>
2019-08-27 19:09:47 +0300

    fit B

--- Radiation/main.cpp
+++ Radiation/main.cpp
@@ -33,6 +33,30 @@ const double UvarovX[Napprox] = {
 	1.8, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 12.0
 };
 
+const double augx[5] = {0.335, 0.625, 1.46, 4.92, 8.57};
+const double augy[5] = {3.29, 7.77, 8.53, 2.42, 1.06};
+
+const double augmaxx = 0.886;
+const double augmaxy = 11.2;
+
+const double junx[4] = {0.628, 1.45, 4.89, 8.53};
+const double juny[4] = {2.98, 12.3, 5.79, 3.15};
+
+const double junmaxx = 1.65;
+const double junmaxy = 13.2;
+
+const double mayx[3]= {1.46, 4.94, 8.62};
+const double mayy[3] = {4.91, 12.0, 6.67};
+
+const double maymaxx = 2.96;
+const double maymaxy = 15.2;
+
+const double aprx[4] = {1.44, 4.91, 8.58, 22.8};
+const double apry[4] ={0.993, 13.9, 17.1, 5.11};
+
+const double aprmaxx = 6.50;
+const double aprmaxy = 19.3;
+
 double power(const double& v, const double& p) {
 	return exp(p * log(v));
 }
@@ -89,6 +113,61 @@ double evaluateMcDonaldFunction(const double& nu) {
 	return result;
 }
 
+void evaluateSpectrum(double* nu, double* Inu, double* Anu, int Nnu, double* Ee, double* Fe, int Np, double minEnergy, double maxEnergy, int startElectronIndex, double sinhi, double Bmean, double concentration, double localSize) {
+	double minNu = 0.001 * criticalNu(minEnergy, sinhi, Bmean);
+	double maxNu = 10 * criticalNu(maxEnergy, sinhi, Bmean);
+
+	double temp = maxNu / minNu;
+
+	double factor = power(maxNu / minNu, 1.0 / (Nnu - 1));
+
+	nu[0] = minNu;
+	Inu[0] = 0;
+	Anu[0] = 0;
+	for (int i = 1; i < Nnu; ++i) {
+		nu[i] = nu[i - 1] * factor;
+		Inu[i] = 0;
+		Anu[i] = 0;
+	}
+
+	double coef = concentration * sqrt(3.0) * electron_charge * electron_charge * electron_charge / (massElectron * speed_of_light2);
+	double coefAbsorb = concentration * 16 * pi * pi * electron_charge / (3 * sqrt(3.0) * Bmean * sinhi);
+
+
+	for (int i = 0; i < Nnu; ++i) {
+		printf("i = %d\n", i);
+		for (int j = startElectronIndex; j < Np; ++j) {
+			double nuc = criticalNu(Ee[j], sinhi, Bmean);
+			double gamma = Ee[j] / (massElectron * speed_of_light2);
+			double x = nu[i] / nuc;
+			Inu[i] = Inu[i] + coef * Fe[j] * (Ee[j] - Ee[j - 1]) * Bmean * sinhi * evaluateMcDonaldIntegral(nu[i] / nuc);
+			Anu[i] = Anu[i] + coefAbsorb * Fe[j] * (Ee[j] - Ee[j - 1]) * evaluateMcDonaldFunction(nu[i] / nuc) / (gamma * gamma * gamma * gamma * gamma);
+		}
+	}
+	int absorbtionIndex = 0;
+	for (int i = 0; i < Nnu; ++i) {
+		if (Anu[i] * localSize < 1) {
+			absorbtionIndex = i;
+			break;
+		}
+	}
+
+	for (int i = 0; i < Nnu; ++i) {
+		Inu[i] = Inu[i] * (1 - exp(-Anu[i] * localSize)) / (Anu[i] * localSize);
+	}
+}
+
+void findMaxNu(int& nuMaxIndex, double* Inu, int Nnu) {
+	double Imax = 0;
+	nuMaxIndex = 0;
+	for(int i = 0; i < Nnu; ++i) {
+		if(Inu[i] > Imax) {
+			Imax = Inu[i];
+			nuMaxIndex = i;
+		}
+	}
+}
+
 int main(int argc, char** argv) {
 
 	int Np = 200;
@@ -105,18 +184,21 @@ int main(int argc, char** argv) {
 	double* Anu = new double[Nnu];
 
 	double sigma = 0.36;
-	double gamma0 = 1.5;
+	double gamma0 = 1.2;
 	double v = speed_of_light * sqrt(1 - 1 / (gamma0 * gamma0));
 	const int Npoints = 4;
 
 
 	double B0 = 0.4;
-	double n0 = 50;
+	double n0 = 5;
 	double L0 = 1E16;
 
-	double L3 = 2.5E17;
+	double L3 = 2.16E17;
 	double n3 = 1;
-	double B3 = 0.045;
+	double B3 = 0.1;
+
+	double B3min = 0.01;
+	double B3max = 100;
 
 	double size[Npoints];
 	double B[Npoints];
@@ -195,64 +277,59 @@ int main(int argc, char** argv) {
 	double hi = pi / 4;
 	double sinhi = sin(hi);
 
+	bool converges = false;
+	double leftB = B3min;
+	double rightB = B3max;
+	double localConcentration = n[3];
+	double localSize = size[3];
+	double localB = (leftB + rightB)/2;
+	evaluateSpectrum(nu, Inu, Anu, Nnu, Ee, Fe, Np, minEnergy, maxEnergy, startElectronIndex, sinhi, localB, localConcentration, localSize);
+	double nuMax = 0;
+	int nuMaxIndex = 0;
+	findMaxNu(nuMaxIndex, Inu, Nnu);
+	nuMax = nu[nuMaxIndex];
+	int iterations = 0;
+	double factor = 1;
+	while(!converges) {
+		iterations++;
+		if(nuMax > augmaxx*1E9) {
+			rightB = localB;
+			localB = (leftB + rightB)/2;
+			evaluateSpectrum(nu, Inu, Anu, Nnu, Ee, Fe, Np, minEnergy, maxEnergy, startElectronIndex, sinhi, localB, localConcentration, localSize);
+			findMaxNu(nuMaxIndex, Inu, Nnu);
+		} else {
+			leftB = localB;
+			localB = (leftB + rightB)/2;
+			evaluateSpectrum(nu, Inu, Anu, Nnu, Ee, Fe, Np, minEnergy, maxEnergy, startElectronIndex, sinhi, localB, localConcentration, localSize);
+			findMaxNu(nuMaxIndex, Inu, Nnu);
+		}
+		nuMax = nu[nuMaxIndex];
+		if((rightB - leftB) < 0.0001) {
+			converges = true;
+		}
+	}
+	factor = augmaxy/Inu[nuMaxIndex];
+
+	printf("B august = %g\n", localB);
+	B[3] = localB;
+	for (int i = 0; i < Npoints-1; ++i) {
+		B[i] = B[3] * size[3] / size[i];
+	}
+
+
 	for (int k = 0; k < Npoints; ++k) {
 
 		double Bmean = B[k];
 
-		double concentration = n[k];
+		localConcentration = n[k];
+		localSize = size[k];
 
 
-		double minNu = 0.001 * criticalNu(minEnergy, sinhi, Bmean);
-		double maxNu = 10 * criticalNu(maxEnergy, sinhi, Bmean);
-		double meanNu = criticalNu(meanE, sinhi, Bmean);
-
-		double temp = maxNu / minNu;
-
-		double factor = power(maxNu / minNu, 1.0 / (Nnu - 1));
-
-		nu[0] = minNu;
-		Inu[0] = 0;
-		Anu[0] = 0;
-		for (int i = 1; i < Nnu; ++i) {
-			nu[i] = nu[i - 1] * factor;
-			Inu[i] = 0;
-			Anu[i] = 0;
-		}
-
-		double coef = concentration * sqrt(3.0) * electron_charge * electron_charge * electron_charge / (massElectron * speed_of_light2);
-		double coefAbsorb = concentration * 16 * pi * pi * electron_charge / (3 * sqrt(3.0) * Bmean * sinhi);
-
-
-		for (int i = 0; i < Nnu; ++i) {
-			printf("i = %d\n", i);
-			for (int j = startElectronIndex; j < Np; ++j) {
-				double nuc = criticalNu(Ee[j], sinhi, Bmean);
-				double gamma = Ee[j] / (massElectron * speed_of_light2);
-				double x = nu[i] / nuc;
-				Inu[i] = Inu[i] + coef * Fe[j] * (Ee[j] - Ee[j - 1]) * Bmean * sinhi * evaluateMcDonaldIntegral(nu[i] / nuc);
-				Anu[i] = Anu[i] + coefAbsorb * Fe[j] * (Ee[j] - Ee[j - 1]) * evaluateMcDonaldFunction(nu[i] / nuc) / (gamma * gamma * gamma * gamma * gamma);
-			}
-		}
-
-		int absorbtionIndex = 0;
-		for (int i = 0; i < Nnu; ++i) {
-			if (Anu[i] * size[k] < 1) {
-				absorbtionIndex = i;
-				break;
-			}
-		}
-
-		/*for(int i = 0; i < absorbtionIndex; ++i) {
-			Inu[i] = Inu[absorbtionIndex]*power(nu[i]/nu[absorbtionIndex], 5.0/2.0);
-		}*/
-
-		for (int i = 0; i < Nnu; ++i) {
-			Inu[i] = Inu[i] * (1 - exp(-Anu[i] * size[k])) / (Anu[i] * size[k]);
-		}
+		evaluateSpectrum(nu, Inu, Anu, Nnu, Ee, Fe, Np, minEnergy, maxEnergy, startElectronIndex, sinhi, Bmean, localConcentration, localSize);
 
 		double totalFlux = 0;
 		for (int i = 1; i < Nnu; ++i) {
-			totalFlux += Inu[i] * (nu[i] - nu[i - 1]) * size[k] * size[k] * size[k];
+			totalFlux += Inu[i] * (nu[i] - nu[i - 1]) * localSize * localSize * localSize;
 		}
 
 
@@ -264,8 +341,8 @@ int main(int argc, char** argv) {
 		FILE* output = fopen((fileName + fileNumber + ".dat").c_str(), "w");
 		delete[] number;
 		for (int i = 0; i < Nnu; ++i) {
-			double totalInu = Inu[i]*size[k]*size[k]*size[k]/(size[0]*size[0]*size[0]);
-			fprintf(output, "%g %g %g %g\n", nu[i]/1E9, Inu[i], Anu[i] * size[k], totalInu);
+			double totalInu = Inu[i]*localSize*localSize*localSize/(size[3]*size[3]*size[3]);
+			fprintf(output, "%g %g %g %g %g %g\n", nu[i]/1E9, Inu[i], Anu[i] * localSize, totalInu, Inu[i]*factor, totalInu*factor);
 		}
 
 		fclose(output);
