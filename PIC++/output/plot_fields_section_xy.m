ed018caebdc06a6bd3a21dc42f689abe66557898
vadimromansky <romanskyvadim@gmail.com>
2020-10-05 17:15:35 +0300

    matlan fields output for buneman bug fixed

--- PIC++/output/plot_fields_section_xy.m
+++ PIC++/output/plot_fields_section_xy.m
@@ -10,9 +10,10 @@ Nx = size(Xfile, 1);
 Ny = size(Yfile, 1);
 Nz = size(Zfile, 1);
 
-NE = Nx*Ny;
+%for buneman -1;
+NE = (Nx-1)*(Ny-1);
 NB = (Nx-1)*(Ny-1);
-Nt = fix(size(EfieldXY, 1)/NE);
+Nt = size(EfieldXY, 1)/NE;
 NtB = (size(BfieldXY, 1)/NB);
 
 a = 0;
@@ -23,46 +24,46 @@ c = fix(Nt)-1;
 N1 = 1;
 N2 = fix(Nx)-1;
 
-Ex(1:N2-N1+1, 1:Ny) = 0;
-Ey(1:N2-N1+1, 1:Ny) = 0;
-Ez(1:N2-N1+1, 1:Ny) = 0;
+Ex(1:Nx-1, 1:Ny-1) = 0;
+Ey(1:Nx-1, 1:Ny-1) = 0;
+Ez(1:Nx-1, 1:Ny-1) = 0;
 
-Bx(1:N2-N1, 1:Ny-1) = 0;
-By(1:N2-N1, 1:Ny-1) = 0;
-Bz(1:N2-N1, 1:Ny-1) = 0;
+Bx(1:Nx-1, 1:Ny-1) = 0;
+By(1:Nx-1, 1:Ny-1) = 0;
+Bz(1:Nx-1, 1:Ny-1) = 0;
 
-middleX(1:N2-N1) = 0;
+middleX(1:Nx-1) = 0;
 middleY(1:Ny-1) = 0;
-Xgrid(1:N2-N1+1) = 0;
-Ygrid(1:Ny) = 0;
+Xgrid(1:Nx-1) = 0;
+Ygrid(1:Ny-1) = 0;
 cv = initialParameters(10);
 omega = initialParameters(21);
 omegaElectron = initialParameters(20);
 
 
 
-for i=1:N2-N1+1,
+for i=1:Nx-1,
     %Xgrid(i) = (Xfile(i) - Xfile(2))*omegaElectron/cv;
    Xgrid(i) = (Xfile(i) - Xfile(2));
-    for j = 1:Ny,
-        Ex(i,j) = EfieldXY((Ny)*(i+N1-1) + j + c*NE, 1);
-        Ey(i,j) = EfieldXY((Ny)*(i+N1-1) + j + c*NE, 2);
-        Ez(i,j) = EfieldXY((Ny)*(i+N1-1) + j + c*NE, 3);
+    for j = 1:Ny-1,
+        Ex(i,j) = EfieldXY((Ny-1)*(i-1) + j + c*NE, 1);
+        Ey(i,j) = EfieldXY((Ny-1)*(i-1) + j + c*NE, 2);
+        Ez(i,j) = EfieldXY((Ny-1)*(i-1) + j + c*NE, 3);
     end;
 end;
 
-for j = 1:Ny,
+for j = 1:Ny-1,
     %Ygrid(j) = (Yfile(j) - Yfile(2))*omegaElectron/cv;
    Ygrid(j) = (Yfile(j) - Yfile(2));
 end;
 
-for i = 1:N2-N1,
+for i = 1:Nx-1,
    %middleX(i) = (0.5*(Xfile(i) + Xfile(i+1)) - Xfile(2))*omegaElectron/cv;
    middleX(i) = (0.5*(Xfile(i) + Xfile(i+1)) - Xfile(2));
    for j = 1:Ny-1,
-      Bx(i, j) = BfieldXY(((Ny-1)*(i+N1-1) + j) + c*NB, 1);
-      By(i, j) = BfieldXY(((Ny-1)*(i+N1-1) + j) + c*NB, 2);
-      Bz(i, j) = BfieldXY(((Ny-1)*(i+N1-1) + j) + c*NB, 3);
+      Bx(i, j) = BfieldXY(((Ny-1)*(i-1) + j) + c*NB, 1);
+      By(i, j) = BfieldXY(((Ny-1)*(i-1) + j) + c*NB, 2);
+      Bz(i, j) = BfieldXY(((Ny-1)*(i-1) + j) + c*NB, 3);
    end;
 end;
 
