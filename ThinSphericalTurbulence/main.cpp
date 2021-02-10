2a9a9ffe8e58702d7a8502704daf0052b8e217e5
vadimromansky <romanskyvadim@gmail.com>
2021-02-10 20:33:07 +0300

    some bugs

--- ThinSphericalTurbulence/main.cpp
+++ ThinSphericalTurbulence/main.cpp
@@ -1034,7 +1034,8 @@ int main()
 				tempTotalInu[j][i] = 0;
 			}
 		}
-		fprintf(output, "%g %g %g %g %g %g %g\n", nu[i]/1E9, tempTotalInu[0][i], tempTotalInu[1][i] , tempTotalInu[2][i], tempTotalInu[3][i], tempTotalInu[4][i], tempTotalInu[5][i]);
+		//fprintf(output, "%g %g %g %g %g %g %g\n", nu[i]/1E9, tempTotalInu[0][i], tempTotalInu[1][i] , tempTotalInu[2][i], tempTotalInu[3][i], tempTotalInu[4][i], tempTotalInu[5][i]);
+		fprintf(output, "%g %g %g %g %g\n", nu[i]/1E9, tempTotalInu[0][i], tempTotalInu[1][i] , tempTotalInu[2][i], tempTotalInu[3][i]);
 	}
 
 	fclose(output);
