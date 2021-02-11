ed74c181200e6ded0cc944d473fd8bcc24772373
vadimromansky <romanskyvadim@gmail.com>
2021-02-11 22:40:06 +0300

    start parameters

---
+++ ThinSphericalTurbulence/startparameters.h
@@ -0,0 +1,16 @@
+#ifndef STARTPARAMETERS_H
+#define STARTPARAMETERS_H
+
+const bool parker = false;
+const bool turbulence = false;
+
+enum Geometry {FLAT_SIMPLE, FLAT, SPHERICAL};
+const Geometry geometry = Geometry::SPHERICAL;
+
+enum Input {TRISTAN, SMILEI, MAXWELL, POWERLAW};
+const Input input = Input::SMILEI;
+
+enum Scale {LINEAR, LOG};
+const Scale scale = Scale::LINEAR;
+
+#endif
