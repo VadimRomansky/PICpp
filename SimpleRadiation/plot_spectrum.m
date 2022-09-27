59c0a25bfc1d6d4f5918fc4dde19bd5a1820f16c
vadimromansky <romanskyvadim@gmail.com>
2022-09-27 19:33:10 +0300

    fz

--- SimpleRadiation/plot_spectrum.m
+++ SimpleRadiation/plot_spectrum.m
@@ -1,12 +1,13 @@
 clear;
-
-energy = importdata('Ee3.dat');
-distribution = importdata('Fs3.dat');
+filenumber = 3;
+energy = importdata(strcat('Ee',num2str(filenumber),'.dat'));
+distribution = importdata(strcat('Fs',num2str(filenumber),'.dat'));
 N1 = size(energy,2);
 mp = 100;
 me = 1;
 for i = 1:N1,
-    energy(i) = energy(i)*me;
+    %energy(i) = energy(i)*me;
+    energy(i) = energy(i)*me*sqrt(1800/100);
 end;
 
 distribution2 = importdata('GLE_pdf_sf_B1.dat');
@@ -33,7 +34,7 @@ for i = 1:N3;
         distribution4(i,2) = distribution(140)*power(distribution4(i,1)/(energy(140)+1), -3.5);
     else
         distribution4(i,1) = distribution3(i-N1+36,1);
-        distribution4(i,2) = distribution3(i-N1+36,2)*(distribution4(N1 + 10-1,2))/distribution3(36+10,2);
+        distribution4(i,2) = distribution3(i-N1+36,2)*(distribution4(N1 + 10-1,2))/distribution3(36+10-1,2);
     end
 end;
 
@@ -59,3 +60,8 @@ xlabel ('\gamma');
 ylabel ('F_{\gamma}');
 
 plot(distribution4(1:N3,1), distribution4(1:N3,2), 'red', 'LineWidth',2);
+
+energyOutput(1:N3,1) = 0;
+distributionOutput(1:N3) = 0;
+dlmwrite(strcat('Em',num2str(filenumber),'.dat'),energyOutput, 'delimiter',' ');
+dlmwrite(strcat('Fm',num2str(filenumber),'.dat'),distributionOutput, 'delimiter',' ');
